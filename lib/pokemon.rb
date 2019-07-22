94799ade09d787a75ce6c9274fa1726863a6a260
Kelley Chaplain <kmchaplain@gmail.com>
2019-07-21 22:54:35 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,14 @@
+require 'pry'
 class Pokemon
+  attr_accessor :name, :type, :db
+  attr_reader :id
+
+  def initialize(id:, name:, type:, db:)
+    
+  end
+
+  def self.save(name, type, db)
+    
+  end  
+
 end
